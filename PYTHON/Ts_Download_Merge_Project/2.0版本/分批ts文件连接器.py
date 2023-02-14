12708283f1eed0916cf7762af30bcafe82aa3855
Vh-WmSm <qq1120763331@outlook.com>
2023-02-15 00:04:58 +0800

    Notes_Update_2.15.23_00:04

--- PYTHON/Ts_Download_Merge_Project/2.0版本/分批ts文件连接器.py
+++ PYTHON/Ts_Download_Merge_Project/2.0版本/分批ts文件连接器.py
@@ -3,7 +3,10 @@ import os
 # ts文件超过1000可用此.py工具分批连接ts文件
 path = input('ts文件地址：')
 os.chdir(path)
-start, end = map(int, input('开始编号 结束编号（空格分隔）：').split())
+ts_list = os.listdir()
+ts_list.sort(key=lambda x:int(x[:x.rfind('.')]))
+start = int(ts_list[0].split('.')[0])
+end = int(ts_list[-1].split('.')[0])
 name = input('输出文件名（不带后缀）：')
 s = ''
 count = 0
