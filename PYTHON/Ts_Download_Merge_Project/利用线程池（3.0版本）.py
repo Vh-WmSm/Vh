ab477b5e62b981e4940e3fb444bf01ed69dc3637
Vh-WmSm <qq1120763331@outlook.com>
2023-02-16 19:53:52 +0800

    ts_download_project_update_2

--- PYTHON/Ts_Download_Merge_Project/利用线程池（3.0版本）.py
+++ PYTHON/Ts_Download_Merge_Project/利用线程池（3.0版本）.py
@@ -3,13 +3,12 @@
 import os
 import wget
 import chardet
-import queue
 import concurrent.futures
 
 
 def wget_download(url, file_name):
-    wget.download(url, ts_path + '\\' + file_name)
-    return f"{file_name}下载完成"
+    wget.download(url, ts_path + file_name)
+    return f'{file_name}已下载'
 
 
 def check_encoding(url_txt_path):
@@ -37,18 +36,8 @@ if __name__ == '__main__':
 
     with open(url_txt_path, 'r', encoding=check_encoding(url_txt_path)) as f_r:
         url_list = f_r.read().strip().splitlines()
-    url_queue = queue.Queue()
-    count = 0
-    future_results = []
-    for url in url_list:
-        url_queue.put(url + '\\' + str(count))
-        count += 1
     with concurrent.futures.ThreadPoolExecutor(max_workers=150) as executor:
-        while not url_queue.empty():
-            que = url_queue.get()
-            url = que[:que.rfind('\\')]
-            file_name = que[que.rfind('\\') + 1:] + '.ts'
-            future_result = executor.submit(wget_download, url, '\\' + file_name)  # 按把所有任务提交到线程池
-            future_results.append(future_result)
+        future_results = [executor.submit(wget_download, url, '\\' + f'{count}.ts') for count, url in
+                          enumerate(url_list)]  # 枚举函数，为每个url都对应一个相应的索引，这样就可以代替队列按顺序下载
         for f in concurrent.futures.as_completed(future_results):
             print(f.result())  # 打印的是wget_download的return值
