9b5a164b0430b7e5d37c2d8d96595c17b7a9ac45
Vh-WmSm <qq1120763331@outlook.com>
2023-03-03 21:51:15 +0800

    Linux_Study_Notes_Update

---
+++ NOTES/Linux_Notes/Linux_Git学习笔记目录更新脚本.py
@@ -0,0 +1,34 @@
+import os
+
+os.chdir('C:\\Users\\Vh\\OneDrive\\my_project\\NOTES')
+f_r = open('Linux_Git_Study_Notes.txt', 'r', encoding='utf-8')
+catalogue = '目录：'
+content = '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n'
+while True:
+    line = f_r.readline()
+    try:
+        if line[0] == '~':
+            break
+    except Exception:
+        pass
+while line != '':
+    line = f_r.readline()
+    content += line
+    # 如果是章节里的子标题
+    try:
+        if line[0] == '*' and line[1] != '*':
+            if line[-2] == ':' or line[-2] == '：':
+                catalogue += line[:-2] + '\n'
+            else:
+                catalogue += line
+    # 如果是章节标题
+        elif line[:2] == '**':
+            if line[-2] == ':' or line[-2] == '：':
+                catalogue += '\n' + line[:-2] + '\n'
+            else:
+                catalogue += '\n' + line
+    except Exception:
+        pass
+f_r.close()
+with open('Linux_Git_Study_Notes.txt', 'w', encoding='utf-8') as f_w:
+    f_w.write(catalogue + '\n' + content)
